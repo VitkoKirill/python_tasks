99c099c40cf60ca9f199f75c3b0254c2f7942996
Kirill Vitko <vitkokirill@gmail.com>
2024-12-14 18:56:55 +0300

    uuid and error check fix

--- DictDB/database_dict_repository.py
+++ DictDB/database_dict_repository.py
@@ -1,3 +1,7 @@
+import uuid
+from uuid import UUID
+
+
 class DatabaseDict(object):
     dict_db = {}
 
@@ -6,26 +10,32 @@ class DatabaseDict(object):
             cls.instance = super(DatabaseDict, cls).__new__(cls)
         return cls.instance
 
-    def insert(self, key: str, value: any) -> None:
+    def insert(self, value: any) -> str:
+        key = str(uuid.uuid4())
         if key not in self.dict_db:
             self.dict_db[key] = value
         else:
-            print(f'Insert: Key {key} already exists!')
+            raise KeyError(f'Insert: Key {key} already exist!')
+        return key
 
     def get(self, key: str) -> any:
-        return self.dict_db.get(key)
+        value = self.dict_db.get(key)
+        if value is not None:
+            return value
+        else:
+            raise KeyError(f'Insert: Key {key} does not exist!')
 
     def update(self, key: str, value: any) -> None:
         if key in self.dict_db:
             self.dict_db[key] = value
         else:
-            print(f'Update: Key {key} does not exist!')
+            raise KeyError(f'Update: Key {key} does not exist!')
 
     def delete(self, key: str) -> None:
         try:
             self.dict_db.pop(key)
-        except KeyError:
-            print(f'Delete: Key {key} does not exist!')
+        except:
+            raise KeyError(f'Delete: Key {key} does not exis!')
 
     def select_all(self) -> dict:
         return self.dict_db
--- DictDB/human_service.py
+++ DictDB/human_service.py
@@ -1,19 +1,14 @@
 from DictDB.database_dict_repository import DatabaseDict
 from DictDB.human_model import Human
-import uuid
+
 
 class HumanService:
     def execute(self, name: str, surname: str, age: int, career: str):
         human = Human(name=name, surname=surname, age=age, career=career)
         db = DatabaseDict()
-        uid = HumanService.generate_uuid()
-        db.insert(uid, human)
-        updated_human = db.get(uid)
+        key = db.insert(human)
+        updated_human = db.get(key)
         updated_human.name = 'name'
-        db.update(uid, updated_human)
-        db.delete(uid)
+        db.update(key=key, value=updated_human)
+        db.delete(key)
         print(db.select_all())
-
-    @staticmethod
-    def generate_uuid() -> str:
-        return str(uuid.uuid4())
